bc24a240e64772b6e3dc751aaa4a768c794857ef
rorywilson <rorywilso99@gmail.com>
2022-05-04 19:11:01 +0100

    Removed test.java

--- src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Set;
-
-public class test {
-
-    public static void main(String[] args) {
-        Set<Integer> ints = Set.of(1, 2, 3, 4, 5, 6);
-        System.out.println(ints);
-    }
-}
